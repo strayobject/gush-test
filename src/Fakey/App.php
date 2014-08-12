8a481293e3ba1a3e6d32fd7847376735755bb981
Michal Zdrojewski <code@strayobject.co.uk>
2014-08-12 21:38:54 +0100

    created basic 'app'

---
+++ src/Fakey/App.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fakey;
+
+class App {
+    function init ($id) {
+        if ($id = 1) {
+            // do something
+        }
+    }
+
+    public function run (array $param, $txt)
+    {
+        if (count($param) < 0) {
+            // do something
+        }
+
+        if ($txt == true) {
+            //  do something
+        }
+    }
+}
